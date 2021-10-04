d113e137152e626148edde63632a19fb587d67b7
Nadiia.Zlobynets <euphoria93@gmail.com>
2021-10-04 14:59:44 +0300

    Initial commit

---
+++ web/hello-world.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Hello, World!</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<p style="font-size: 48px;">Hello, World!!</p>
+</body>
+
+</html>
