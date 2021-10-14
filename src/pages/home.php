17e8f5007b833f442f890688c10ace40f818c40d
Nadiia.Zlobynets <euphoria93@gmail.com>
2021-10-14 18:31:21 +0300

    #3 MVC: deleted ./web/data.php, changed formatting in src/pages/home.php

--- src/pages/home.php
+++ src/pages/home.php
@@ -1,34 +1,34 @@
-        <!-- @TODO: Implement recently viewed posts -->
-        <section title="Recently Viewed Posts">
-            <h2>Recently Viewed Posts</h2>
-            <div class="post-list">
-                <div class="post">
-                    <a href="/post-1-url" title="Post 1">
-                        <img src="/post-placeholder.png" alt="Post 1" width="200"/>
-                    </a>
-                    <br>
-                    <a href="/post-1-url" title="Post 1">Post 1</a>
-                    <p>9.04.2022</p>
-                    <p>Nadiia Zlobynets</p>
-                </div>
-                <div class="post">
-                    <a href="/post-2-url" title="Post 2">
-                        <img src="/post-placeholder.png" alt="Post 2" width="200"/>
-                    </a>
-                    <br>
-                    <a href="/post-2-url" title="Post 2">Post 2</a>
-                    <p>22.09.2022</p>
-                    <p>Nadiia Zlobynets</p>
+<!-- @TODO: Implement recently viewed posts -->
+<section title="Recently Viewed Posts">
+    <h2>Recently Viewed Posts</h2>
+    <div class="post-list">
+        <div class="post">
+            <a href="/post-1-url" title="Post 1">
+                <img src="/post-placeholder.png" alt="Post 1" width="200"/>
+            </a>
+            <br>
+            <a href="/post-1-url" title="Post 1">Post 1</a>
+            <p>9.04.2022</p>
+            <p>Nadiia Zlobynets</p>
+        </div>
+        <div class="post">
+            <a href="/post-2-url" title="Post 2">
+                <img src="/post-placeholder.png" alt="Post 2" width="200"/>
+            </a>
+            <br>
+            <a href="/post-2-url" title="Post 2">Post 2</a>
+            <p>22.09.2022</p>
+            <p>Nadiia Zlobynets</p>
 
-                </div>
-                <div class="post">
-                    <a href="/post-3-url" title="Post 3">
-                        <img src="/post-placeholder.png" alt="Post 3" width="200"/>
-                    </a>
-                    <br>
-                    <a href="/post-3-url" title="Post 3">Post 3</a>
-                    <p>11.04.2022</p>
-                    <p>Nadiia Zlobynets</p>
-                </div>
-            </div>
-        </section>
+        </div>
+        <div class="post">
+            <a href="/post-3-url" title="Post 3">
+                <img src="/post-placeholder.png" alt="Post 3" width="200"/>
+            </a>
+            <br>
+            <a href="/post-3-url" title="Post 3">Post 3</a>
+            <p>11.04.2022</p>
+            <p>Nadiia Zlobynets</p>
+        </div>
+    </div>
+</section>
