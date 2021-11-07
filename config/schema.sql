c0b614e04eb72ad16b743c97ee4aa898df02fd60
Nadiia.Zlobynets <euphoria93@gmail.com>
2021-11-07 21:47:35 +0200

    #5 Creating a MySQL relational database: added author_post and category_post

--- config/schema.sql
+++ config/schema.sql
@@ -1,3 +1,5 @@
+DROP TABLE IF EXISTS `daily_statistics`;
+#---
 DROP TABLE IF EXISTS `category_post`;
 #---
 DROP TABLE IF EXISTS `author_post`;
@@ -14,7 +16,7 @@ CREATE TABLE `post` (
    `url` varchar(127) NOT NULL COMMENT 'URL',
    `description` varchar(4095) DEFAULT NULL COMMENT 'Description',
    `date` int unsigned NOT NULL COMMENT 'Date',
-   PRIMARY KEY (`post_id`)
+    PRIMARY KEY (`post_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='Post Entity';
 #---
 CREATE TABLE `category` (
@@ -28,25 +30,61 @@ CREATE TABLE `author` (
      `author_id` int unsigned NOT NULL AUTO_INCREMENT COMMENT 'Author ID',
      `firstname` varchar(127) DEFAULT NULL COMMENT 'First Name',
      `lastname` varchar(127) DEFAULT NULL COMMENT 'Last Name',
-     `url` varchar(127) NOT NULL COMMENT 'URL',
-     `image` varchar(127) NOT NULL COMMENT 'IMG',
+     `url` varchar(127) DEFAULT NULL COMMENT 'URL',
+     `image` varchar(127) DEFAULT NULL COMMENT 'IMG',
      `description` varchar(4095) DEFAULT NULL COMMENT 'Description',
      PRIMARY KEY (`author_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='Order Entity';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='Author Entity';
 #---
 CREATE TABLE `author_post` (
     `author_post_id` int unsigned NOT NULL AUTO_INCREMENT COMMENT 'ID',
-    `author_id` int unsigned NOT NULL COMMENT 'Author ID',
-    `post_id` int unsigned NOT NULL COMMENT 'Post ID',
+    `author_id` int unsigned DEFAULT NULL COMMENT 'Author ID',
+    `post_id` int unsigned DEFAULT NULL COMMENT 'Post ID',
     PRIMARY KEY (`author_post_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='Author Post';
 #---
+ALTER TABLE `author_post`
+    ADD CONSTRAINT `FK_AUTHOR_POST_AUTHOR_ID` FOREIGN KEY `author` (`author_id`)
+        REFERENCES `author` (`author_id`) ON DELETE CASCADE,
+    ADD CONSTRAINT `FK_AUTHOR_POST_POST_ID` FOREIGN KEY `post `(`post_id`)
+        REFERENCES `post` (`post_id`) ON DELETE CASCADE;
+#---
+-- INSERT INTO `author_post` (`author_id`, `post_id`)
+-- VALUES (1, 15), (2, 14), (3, 13), (4, 12),
+--        (5, 11), (6, 10), (7, 9), (8, 8),
+--        (9, 4), (10, 6, (11, 6), (12, 7),
+--        (13, 3), (14, 2), (15, 1);
+-- #---
 CREATE TABLE `category_post` (
     `category_post_id` int unsigned NOT NULL AUTO_INCREMENT COMMENT 'ID',
     `post_id` int unsigned NOT NULL COMMENT 'Post ID',
     `category_id` int unsigned NOT NULL COMMENT 'Category ID',
     PRIMARY KEY (`category_post_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='Category Post';
+#---
+ALTER TABLE `category_post`
+    ADD CONSTRAINT `FK_CATEGORY_POST_CATEGORY_ID` FOREIGN KEY `category` (`category_id`)
+        REFERENCES `category` (`category_id`) ON DELETE CASCADE,
+    ADD CONSTRAINT `FK_CATEGORY_POST_POST_ID` FOREIGN KEY `post` (`post_id`)
+        REFERENCES `post` (`post_id`) ON DELETE CASCADE;
+#---
+CREATE TABLE `daily_statistics` (
+    `daily_statistics_id` int unsigned NOT NULL AUTO_INCREMENT COMMENT 'ID',
+    `post_id` int unsigned NOT NULL COMMENT 'Post ID',
+    `date` int unsigned NOT NULL COMMENT 'Date',
+    `views` smallint DEFAULT NULL COMMENT 'Views',
+    PRIMARY KEY (`daily_statistics_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='Daily Statistics';
+#---
+-- ALTER TABLE `daily_statistics`
+--     ADD CONSTRAINT `FK_DAILY_STATISTICS_ID` FOREIGN KEY (`daily_statistics_id`)
+--         REFERENCES daily_statistics(`daily_statistics_id`) ON DELETE CASCADE,
+--     ADD CONSTRAINT `FK_DAILY_STATISTICS_POST_ID` FOREIGN KEY `post_id`
+--         REFERENCES `post` (`post_id`) ON DELETE CASCADE;
+-- #---
+-- INSERT INTO `daily_statistics` (`post_id`, `date`, views)
+-- VALUES (1, 1636126765, 21),
+--        (2, 1526729821, 15)
 #---
 INSERT INTO `post` (`name`, `url`, `description`, `date`)
 VALUES ('Post 1', 'post-1', 'Lorem ipsum dolor sit amet', 1636126765),
